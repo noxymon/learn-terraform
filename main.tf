e0bbbb5ce888896fa2965fa817897ac163651051
noxymon <avicienna.ulhaq@gmail.com>
2023-08-10 16:40:06 +0700

    add kubernetes provider

--- main.tf
+++ main.tf
@@ -71,10 +71,16 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 module "eks-kubeconfig" {
   source  = "hyperbadger/eks-kubeconfig/aws"
   version = "2.0.0"
-  
+
   cluster_name = module.eks.cluster_id
 }
 
