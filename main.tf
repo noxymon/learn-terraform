e34a7fd0cf4183489302149fb99275aa984d0bbe
noxymon <avicienna.ulhaq@gmail.com>
2023-08-10 14:34:30 +0700

    add 2 availability zone

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "vpc" {
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  azs             = ["ap-southeast-3a"]
+  azs             = ["ap-southeast-3a", "ap-southeast-3b"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
